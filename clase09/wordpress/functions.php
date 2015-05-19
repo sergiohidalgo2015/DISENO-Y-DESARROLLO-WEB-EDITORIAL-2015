0e5ed9fc2e4d09199256cdac054416ac0bbe9b12
Sergio <sergiohidalgocaceres@gmail.com>
2015-05-18 22:03:48 -0500

    Clase 09

--- clase09/wordpress/functions.php
+++ clase09/wordpress/functions.php
@@ -1,4 +1,14 @@
 <?php
 	define("RUTA_BLOG",   get_bloginfo("stylesheet_directory") );
 	define("RUTA_ELEMENTOS", RUTA_BLOG. "/elementos");
+
+	//Este código nos crea el soporte para menus
+	add_theme_support("nav-menus");
+	if(function_exists("register_nav_menus")){
+		register_nav_menus(
+			array("principal" => "Menu Principal", "secundario" => "Menu Secundario")
+		);
+	}
+	///////////////////////////////////////////////
+	
 ?>
