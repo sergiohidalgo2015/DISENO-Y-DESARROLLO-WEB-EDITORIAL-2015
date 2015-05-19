91426b38f4fc71d70115417dee920d839f6f68be
Sergio <sergiohidalgocaceres@gmail.com>
2015-05-18 21:12:45 -0500

    Clase 09

--- clase09/wordpress/header.php
+++ clase09/wordpress/header.php
@@ -14,40 +14,40 @@
 <meta name="GOOGLEBOT" content="NOODP">
 <meta name="googlebot" content="noodp">
 <title>Ambulancias Perú - Misión Médica</title>
-<link rel="stylesheet" type="text/css" href="elementos/estilos/fuentes.css" media="screen">
-<link rel="stylesheet" type="text/css" href="elementos/estilos/principal.css" media="screen">
-<link rel="stylesheet" type="text/css" href="elementos/estilos/menu.css" media="screen">
-<link rel="stylesheet" type="text/css" href="elementos/estilos/home.css" media="screen">
-<!-- <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script><script>window.jQuery || document.write('<script src="http://www.tibajodemanda.com/elementos/js/jquery/jquery-1.7.1.min.js">\x3C/script>')</script>-->
+<link rel="stylesheet" type="text/css" href="<?php print RUTA_ELEMENTOS ?>/estilos/fuentes.css" media="screen">
+<link rel="stylesheet" type="text/css" href="<?php print RUTA_ELEMENTOS ?>/estilos/principal.css" media="screen">
+<link rel="stylesheet" type="text/css" href="<?php print RUTA_ELEMENTOS ?>/estilos/menu.css" media="screen">
+<link rel="stylesheet" type="text/css" href="<?php print RUTA_ELEMENTOS ?>/estilos/home.css" media="screen">
+<!-- <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script><script>window.jQuery || document.write('<script src="http://www.tibajodemanda.com/<?php print RUTA_ELEMENTOS ?>/js/jquery/jquery-1.7.1.min.js">\x3C/script>')</script>-->
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
-<script type="text/javascript" src="elementos/js/principal.js"></script>
-<script type="text/javascript" src="elementos/js/menu.js"></script>
-<script type="text/javascript" src="elementos/js/jquery/cycle/jquery.cycle.all.js"></script>
+<script type="text/javascript" src="<?php print RUTA_ELEMENTOS ?>/js/principal.js"></script>
+<script type="text/javascript" src="<?php print RUTA_ELEMENTOS ?>/js/menu.js"></script>
+<script type="text/javascript" src="<?php print RUTA_ELEMENTOS ?>/js/jquery/cycle/jquery.cycle.all.js"></script>
 <!--[if lt IE 9]>       <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>   <![endif]-->
 <article id="contenedor">
     <header>
         <nav id="menuSuperior">
             <ul>
-                <li id="sp1"><a href="familia-mision-medica.html"><span><img src="elementos/imagenes/vineta_superior.png"></span>&nbsp;Se parte de la Familia Misión Médica</a></li>
-                <li id="sp2"><a href="contactos.html"><span><img src="elementos/imagenes/vineta_superior.png"></span>&nbsp;Contáctenos</a></li>
-                <li id="sp3"><a href="mapa.html"><span><img src="elementos/imagenes/vineta_superior.png"></span>&nbsp;Mapa del Sitio</a></li>
-                <li id="sp4"><a href="#"><span><img src="elementos/imagenes/vineta_superior.png"></span>&nbsp;Acceso a Intranet</a></li>
+                <li id="sp1"><a href="familia-mision-medica.html"><span><img src="<?php print RUTA_ELEMENTOS ?>/imagenes/vineta_superior.png"></span>&nbsp;Se parte de la Familia Misión Médica</a></li>
+                <li id="sp2"><a href="contactos.html"><span><img src="<?php print RUTA_ELEMENTOS ?>/imagenes/vineta_superior.png"></span>&nbsp;Contáctenos</a></li>
+                <li id="sp3"><a href="mapa.html"><span><img src="<?php print RUTA_ELEMENTOS ?>/imagenes/vineta_superior.png"></span>&nbsp;Mapa del Sitio</a></li>
+                <li id="sp4"><a href="#"><span><img src="<?php print RUTA_ELEMENTOS ?>/imagenes/vineta_superior.png"></span>&nbsp;Acceso a Intranet</a></li>
             </ul>
         </nav>
     </header>
     <section id="accesoIntranet"></section>
     <section id="contenedorIconos">
         <div id="logoMM">
-            <a href="index.html"><img src="elementos/imagenes/logoMisionMedica.gif"></a>
+            <a href="index.html"><img src="<?php print RUTA_ELEMENTOS ?>/imagenes/logoMisionMedica.gif"></a>
         </div>
         <div id="logoLK">
-            <a href="http://www.linkedin.com/company/mision-medica" target="_blank"><img src="elementos/imagenes/logoLinkeIn.jpg"></a>
+            <a href="http://www.linkedin.com/company/mision-medica" target="_blank"><img src="<?php print RUTA_ELEMENTOS ?>/imagenes/logoLinkeIn.jpg"></a>
         </div>
         <div id="logoTW">
-            <a href="https://twitter.com/misionmedicasac" target="_blank"><img src="elementos/imagenes/logoTwitter.jpg"></a>
+            <a href="https://twitter.com/misionmedicasac" target="_blank"><img src="<?php print RUTA_ELEMENTOS ?>/imagenes/logoTwitter.jpg"></a>
         </div>
         <div id="logoFB">
-            <a href="http://www.facebook.com/Mision.Medica" target="_blank"><img src="elementos/imagenes/logoFacebook.jpg"></a>
+            <a href="http://www.facebook.com/Mision.Medica" target="_blank"><img src="<?php print RUTA_ELEMENTOS ?>/imagenes/logoFacebook.jpg"></a>
         </div>
     </section>
     <section id="menuPrincipal">
