5f53ff2eab55d8d8d216f54a7a6c6390a1ee8ad4
Sergio <sergiohidalgocaceres@gmail.com>
2015-05-18 21:00:38 -0500

    Clase 09

---
+++ clase09/wordpress/footer.php
@@ -0,0 +1,21 @@
+    <div id="menuInferior">
+        <nav>
+            <ul>
+                <li><a href="nuestroslocales.html"><span><img src="elementos/imagenes/vineta.png"></span>&nbsp;Nuestros locales</a></li>
+                <li><a href="informacion.html"><span><img src="elementos/imagenes/vineta.png"></span>&nbsp;Información útil</a></li>
+                <li><a href="#"><span><img src="elementos/imagenes/vineta.png"></span>&nbsp;Preguntas frecuentes</a></li>
+            </ul>
+        </nav>
+    </div>
+    <footer> <address id="piePagina">Av. Javier Prado Este 3542, San Borja. Lima<br>@2012 MISIÓN MÉDICA - Todos los derechos reservados</address>
+        <figure id="logoUKAS"><img src="elementos/imagenes/logoUKAS.png"></figure>
+        <figure id="logoISO"><img src="elementos/imagenes/logoISO.png"></figure>
+        <figure id="selloMapfre">
+            <div align="center">
+                <a href="http://www.mapfreperu.com" target="_blank"> <img src="elementos/imagenes/sello_mapfre.jpg" alt="Clínica afiliada a Mapfre"> </a>
+            </div>
+        </figure>
+    </footer>
+</article>
+
+</html>
---
+++ clase09/wordpress/header.php
@@ -0,0 +1,61 @@
+<html>
+<!--[if lt IE 7]>  <html class="no-js ie6 oldie" lang="es"> <script>window.ie = 6;</script> <![endif]-->
+<!--[if IE 7]>     <html class="no-js ie7 oldie" lang="es"> <script>window.ie = 7;</script> <![endif]-->
+<!--[if IE 8]>     <html class="no-js ie8 oldie" lang="es"> <script>window.ie = 8;</script> <![endif]-->
+<!--[if gt IE 8]>  <html class="no-js ie9" lang="es"> <script>window.ie = 9;</script> <![endif]-->
+<!--[if !IE]><!-->
+<!--<![endif]-->
+<meta charset="iso-8859-1">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<meta name="description" content="">
+<meta name="viewport" content="width=device-width,initial-scale=1">
+<meta name="author" content="Sergio Hidalgo - shidalgo@tibajodemanda.com">
+<meta name="author URL" content="http://www.tibajodemanda.com/">
+<meta name="GOOGLEBOT" content="NOODP">
+<meta name="googlebot" content="noodp">
+<title>Ambulancias Perú - Misión Médica</title>
+<link rel="stylesheet" type="text/css" href="elementos/estilos/fuentes.css" media="screen">
+<link rel="stylesheet" type="text/css" href="elementos/estilos/principal.css" media="screen">
+<link rel="stylesheet" type="text/css" href="elementos/estilos/menu.css" media="screen">
+<link rel="stylesheet" type="text/css" href="elementos/estilos/home.css" media="screen">
+<!-- <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script><script>window.jQuery || document.write('<script src="http://www.tibajodemanda.com/elementos/js/jquery/jquery-1.7.1.min.js">\x3C/script>')</script>-->
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+<script type="text/javascript" src="elementos/js/principal.js"></script>
+<script type="text/javascript" src="elementos/js/menu.js"></script>
+<script type="text/javascript" src="elementos/js/jquery/cycle/jquery.cycle.all.js"></script>
+<!--[if lt IE 9]>       <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>   <![endif]-->
+<article id="contenedor">
+    <header>
+        <nav id="menuSuperior">
+            <ul>
+                <li id="sp1"><a href="familia-mision-medica.html"><span><img src="elementos/imagenes/vineta_superior.png"></span>&nbsp;Se parte de la Familia Misión Médica</a></li>
+                <li id="sp2"><a href="contactos.html"><span><img src="elementos/imagenes/vineta_superior.png"></span>&nbsp;Contáctenos</a></li>
+                <li id="sp3"><a href="mapa.html"><span><img src="elementos/imagenes/vineta_superior.png"></span>&nbsp;Mapa del Sitio</a></li>
+                <li id="sp4"><a href="#"><span><img src="elementos/imagenes/vineta_superior.png"></span>&nbsp;Acceso a Intranet</a></li>
+            </ul>
+        </nav>
+    </header>
+    <section id="accesoIntranet"></section>
+    <section id="contenedorIconos">
+        <div id="logoMM">
+            <a href="index.html"><img src="elementos/imagenes/logoMisionMedica.gif"></a>
+        </div>
+        <div id="logoLK">
+            <a href="http://www.linkedin.com/company/mision-medica" target="_blank"><img src="elementos/imagenes/logoLinkeIn.jpg"></a>
+        </div>
+        <div id="logoTW">
+            <a href="https://twitter.com/misionmedicasac" target="_blank"><img src="elementos/imagenes/logoTwitter.jpg"></a>
+        </div>
+        <div id="logoFB">
+            <a href="http://www.facebook.com/Mision.Medica" target="_blank"><img src="elementos/imagenes/logoFacebook.jpg"></a>
+        </div>
+    </section>
+    <section id="menuPrincipal">
+        <ul id="menu">
+            <li id="m1"><a href="nosotros.html">Nosotros</a></li>
+            <li id="m2"><a href="servicios.html">Nuestros servicios</a></li>
+            <li id="m3"><a href="personal.html">Nuestro personal</a></li>
+            <li id="m4"><a href="clientes.html">Nuestros clientes</a></li>
+            <li id="m5"><a href="trujillo.html" class="itemFinal">Misión Médica en Trujillo</a></li>
+        </ul>
+    </section>
